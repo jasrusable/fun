8b0b31c2fddd3f528c1be366cb9495e92a6b005d
Jason Russell <jasrusable@gmail.com>
2014-02-15 16:59:59 +0200

    start coding test_index method

--- jason/test_index.py
+++ jason/test_index.py
@@ -1,5 +1,4 @@
 from flask.ext.testing import TestCase, Twill
-from bs4 import Beautifulsoupo
 from jason import app
 
 
@@ -8,3 +7,7 @@ class TestIndex(TestCase):
 	def create_app(self):
 		app.config['TESTING'] = True
 		return app
+
+	def test_index(self):
+		with Twill(self.app, port=5000) as t:
+			t.browser.go(t.url('/'))
