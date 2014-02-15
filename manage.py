4a88eb1ad0e7a3df09a60c8fae8b5601bea53690
Jason Russell <jasrusable@gmail.com>
2014-02-15 16:55:39 +0200

    add test option

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 from flask.ext.script import Manager
 from jason import app
-
+import nose
 
 manager = Manager(app)
+
+def test():
+	nose.main(argv=['jason', '--failed'])
 
 def main():
 	manager.run()
