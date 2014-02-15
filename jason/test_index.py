007cb45f25a33c7288f2e9421dcda32c61ef624a
Jason Russell <jasrusable@gmail.com>
2014-02-15 16:52:46 +0200

    add create_app method

--- jason/test_index.py
+++ jason/test_index.py
@@ -1,3 +1,10 @@
 from flask.ext.testing import TestCase, Twill
 from bs4 import Beautifulsoupo
+from jason import app
 
+
+class TestIndex(TestCase):
+	""" Test idex """
+	def create_app(self):
+		app.config['TESTING'] = True
+		return app
